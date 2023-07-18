e2106e35b983e9462e74d3b21e61bea5d2e9a154
Garrett Arnold <garrettmarnold@gmail.com>
2023-07-18 10:20:33 +0200

    create week 2 file

---
+++ week-2/day-6/first-bootstrap-page/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css';  
+
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <div className="text-center pt-5">
+      <h1>First React App</h1>      
+
+      <Button variant="primary">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
